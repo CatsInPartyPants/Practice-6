878d0bcd7b575645ccb6e832b934222343b59e84
Artem Nilov <nilov1235@mail.ru>
2022-05-22 17:09:10 +0300

    while

--- Practice-6/Practice-6.cpp
+++ Practice-6/Practice-6.cpp
@@ -27,18 +27,85 @@ int main()
 		if (n == 3) {
 			n--;
 			continue;
-		} 
+		}
 		cout << "next ";
 		n--;
 	}
 	cout << n;
 	*/
 
+	/*
 	int number,temp = 0;
 	cout << "Введите число ->";
 	cin >> number;
 	while (temp <= number) {
-		cout << temp << endl;
+		cout << temp++ << endl;
 		temp++;
 	}
+	*/
+
+	/*
+	int n1, n2;
+	cin >> n1 >> n2;
+	int min= min(n1, n2), max = max(n1, n2);
+	int summ = 0;
+	while (min <= max) {
+		summ += min;
+		min++;
+	}
+	cout << summ;
+	*/
+
+	/*
+	int number, result = 0;
+	do {
+		cin >> number;
+		result += number;
+	} while (number != 0 && number != 7);
+
+	cout << result;
+	*/
+
+	/*
+
+	int n1, n2;
+	cout << "Введите первую переменную - >";
+	cin >> n1;
+	cout << "Введите вторую переменную - >";
+	cin >> n2;
+	int min = min(n1, n2), max = max(n1, n2);
+	cout << "Выводим четные:";
+	while (min <= max) {
+		if (min % 2 == 0) cout << min << " ";
+		min++;
+	}
+	min = min(n1, n2);
+	cout << "Выводим нечетные:";
+	while (min <= max) {
+		if (min % 2 != 0) cout << min << " ";
+		min++;
+	}
+	min = min(n1, n2);
+	cout << "Выводим кратные 7:";
+		while (min <= max) {
+		if (min % 7 == 0) cout << min << " ";
+		min++;
+	}
+	*/
+
+	/*
+	int number;
+
+	while (1) {
+		cin >> number;
+		if (number == 0) break;
+	}
+	
+	//ver2
+	do {
+		cin >> number;
+	} while (number);
+	*/
+
+
 }
